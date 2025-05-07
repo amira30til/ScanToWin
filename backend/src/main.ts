642c79ab0853526ecba2e47a79662c9bca195d67
Amine Zouari <zouariamine52@gmail.com>
2025-05-07 11:57:48 +0100

    fix(origin): add swagger url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,8 +20,9 @@ async function bootstrap() {
         process.env.FRONTEND_URL,
         'http://127.0.0.1:5173',
         'http://localhost:5173',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
       ];
-
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
