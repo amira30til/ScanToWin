e86fd22cffbb7014e902a1e2200606c3feec94fe
Amine Zouari <zouariamine52@gmail.com>
2025-05-10 00:52:22 +0100

    feat(auth): reset password

--- frontend/src/components/PersistLogin.jsx
+++ frontend/src/components/PersistLogin.jsx
@@ -1,6 +1,7 @@
+import { useRefreshToken } from "@/hooks";
+import { useMutation } from "@tanstack/react-query";
 import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { useRefreshToken } from "@/hooks";
 import useAuthStore from "@/store";
 import Spinner from "@/components/Spinner";
 
@@ -9,23 +10,15 @@ const PersistLogin = () => {
   const refresh = useRefreshToken();
   const auth = useAuthStore((state) => state.auth);
 
+  const refreshMutation = useMutation({
+    mutationFn: async () => await refresh(),
+    onError: (error) => console.error(error),
+    onSettled: () => setIsLoading(false),
+  });
+
   useEffect(() => {
-    let isMounted = true;
-
-    const verifyRefreshToken = async () => {
-      try {
-        await refresh();
-      } catch (err) {
-        console.error(err);
-      } finally {
-        isMounted && setIsLoading(false);
-      }
-    };
-
     // Always try to refresh if we don't have an access token
-    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
-
-    return () => (isMounted = false);
+    !auth?.accessToken ? refreshMutation.mutate() : setIsLoading(false);
   }, []);
 
   return <>{isLoading ? <Spinner /> : <Outlet />}</>;
