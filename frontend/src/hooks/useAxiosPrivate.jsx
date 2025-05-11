afd665367d77c577616afb150e604b2293ca696d
Amine Zouari <zouariamine52@gmail.com>
2025-05-11 16:26:35 +0100

    feat(auth): test access token

--- frontend/src/hooks/useAxiosPrivate.jsx
+++ frontend/src/hooks/useAxiosPrivate.jsx
@@ -22,7 +22,7 @@ const useAxiosPrivate = () => {
       (response) => response,
       async (error) => {
         const prevRequest = error?.config;
-        if (error?.response?.status === 403 && !prevRequest?.sent) {
+        if (error?.response?.status === 401 && !prevRequest?.sent) {
           prevRequest.sent = true;
           const response = await refresh();
           const newAccessToken = response?.data?.data?.accessToken;
