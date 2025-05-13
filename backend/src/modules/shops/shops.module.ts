8a7c3edf49243c2b6a96fc3e4cafdb1133f73574
amira tilouche <ati@visto-consulting.com>
2025-05-13 17:28:57 +0100

    #WIP

--- backend/src/modules/shops/shops.module.ts
+++ backend/src/modules/shops/shops.module.ts
@@ -5,9 +5,11 @@ import { Shop } from './entities/shop.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Admin } from '../admins/entities/admin.entity';
 import { JwtService } from '@nestjs/jwt';
+import { ChosenGame } from '../chosen-game/entities/chosen-game.entity';
+import { Game } from '../game/entities/game.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Shop, Admin])],
+  imports: [TypeOrmModule.forFeature([Shop, Admin, ChosenGame, Game])],
   controllers: [ShopsController],
   providers: [ShopsService, JwtService],
   exports: [ShopsService],
