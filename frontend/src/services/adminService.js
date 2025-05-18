04834d5c642995771d91507a4c14594a83726d6c
Amine Zouari <zouariamine52@gmail.com>
2025-05-18 01:54:36 +0100

    feat(campaign): create choose game form

--- frontend/src/services/adminService.js
+++ frontend/src/services/adminService.js
@@ -1,12 +1,18 @@
 const ADMIN_URL = "/admins";
+const GAME_URL = "/game";
 
 export const getAdmins = async (axios) => await axios.get(ADMIN_URL);
 
 export const getAdminById = async (axios, id) =>
   await axios.get(`${ADMIN_URL}/${id}`);
 
-export const createAdmin = async (axios, user) =>
-  await axios.post(ADMIN_URL, user);
+export const createAdmin = async (axios, body) =>
+  await axios.post(ADMIN_URL, body);
 
 export const deleteAdmin = async (axios, id) =>
   await axios.delete(`${ADMIN_URL}/${id}`);
+
+export const createGame = async (axios, body) =>
+  await axios.post(GAME_URL, body);
+
+export const getGames = async (axios) => await axios.get(GAME_URL);
