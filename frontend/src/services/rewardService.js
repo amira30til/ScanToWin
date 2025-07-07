fcb67e6f0180ee56fd3ffa03f76d5df9d9de6512
aminezouari52 <zouariamine52@gmail.com>
2025-07-07 14:04:31 +0100

    rewards: upsert

--- frontend/src/services/rewardService.js
+++ frontend/src/services/rewardService.js
@@ -3,5 +3,5 @@ const REWARD_URL = "/reward";
 export const getRewardsByShop = async (axios, shopId) =>
   await axios.get(`${REWARD_URL}/by-shop/${shopId}`);
 
-export const createReward = async (axios, reward) =>
-  await axios.post(REWARD_URL, reward);
+export const upsertRewards = async (axios, rewards) =>
+  await axios.post(REWARD_URL, rewards);
