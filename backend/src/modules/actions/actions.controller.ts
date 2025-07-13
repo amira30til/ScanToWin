329927a414e32da4fb23116fa603f0bcf293f979
Amira Tilouch <tilouchamira@gmail.com>
2025-07-13 19:46:22 +0100

    feat(action/chosen-action): -crud for both modules

--- backend/src/modules/actions/actions.controller.ts
+++ backend/src/modules/actions/actions.controller.ts
@@ -1,34 +1,101 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
 import { ActionsService } from './actions.service';
 import { CreateActionDto } from './dto/create-action.dto';
 import { UpdateActionDto } from './dto/update-action.dto';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+import { ActionMessages } from 'src/common/constants/messages.constants';
+import { Action } from './entities/action.entity';
 
 @Controller('actions')
 export class ActionsController {
   constructor(private readonly actionsService: ActionsService) {}
 
   @Post()
-  create(@Body() createActionDto: CreateActionDto) {
+  @ApiOperation({ summary: 'Create a new action' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: ActionMessages.ACTION_CREATED,
+    type: Action,
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'Action with this name already exists',
+  })
+  async create(@Body() createActionDto: CreateActionDto) {
+    console.log("yyyy", createActionDto);
+    
     return this.actionsService.create(createActionDto);
   }
 
   @Get()
-  findAll() {
+  @ApiOperation({ summary: 'Get all actions' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: ActionMessages.ACTIONS_RETRIEVED,
+    type: [Action],
+  })
+  async findAll() {
     return this.actionsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.actionsService.findOne(+id);
+  @ApiOperation({ summary: 'Get action by ID' })
+  @ApiParam({ name: 'id', type: String, description: 'UUID of the action' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: ActionMessages.ACTION_RETRIEVED,
+    type: Action,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Action not found',
+  })
+  async findOne(@Param('id') id: string) {
+    return this.actionsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateActionDto: UpdateActionDto) {
-    return this.actionsService.update(+id, updateActionDto);
+  @ApiOperation({ summary: 'Update an action by ID' })
+  @ApiParam({ name: 'id', type: String, description: 'UUID of the action' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: ActionMessages.ACTION_UPDATED,
+    type: Action,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Action not found',
+  })
+  async update(
+    @Param('id') id: string,
+    @Body() updateActionDto: UpdateActionDto,
+  ) {
+    return this.actionsService.update(id, updateActionDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.actionsService.remove(+id);
+  @ApiOperation({ summary: 'Delete an action by ID' })
+  @ApiParam({ name: 'id', type: String, description: 'UUID of the action' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: ActionMessages.ACTION_DELETED,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Action not found',
+  })
+  @HttpCode(HttpStatus.OK)
+  async remove(@Param('id') id: string) {
+    return this.actionsService.remove(id);
   }
 }
