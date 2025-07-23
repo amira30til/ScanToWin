52ea0651c96f2ce7e48e954ce2dc93688cf6b5a7
Amira Tilouch <tilouchamira@gmail.com>
2025-07-23 20:45:55 +0100

    fix(click action +redemption): add JwtService

--- backend/src/modules/action-click/action-click.module.ts
+++ backend/src/modules/action-click/action-click.module.ts
@@ -5,11 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActionClick } from './entities/action-click.entity';
 import { Shop } from '../shops/entities/shop.entity';
 import { ChosenAction } from '../chosen-action/entities/chosen-action.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ActionClick, Shop, ChosenAction])],
   controllers: [ActionClickController],
-  providers: [ActionClickService],
+  providers: [ActionClickService,JwtService],
   exports: [ActionClickService],
 })
 export class ActionClickModule {}
