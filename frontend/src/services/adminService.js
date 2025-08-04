7b7cf0a5f434a53ee486da909ab3591506e957dd
aminezouari52 <zouariamine52@gmail.com>
2025-08-04 19:45:38 +0100

    feat: admin

--- frontend/src/services/adminService.js
+++ frontend/src/services/adminService.js
@@ -3,11 +3,16 @@ const ADMIN_URL = "/admins";
 export const getAdmins = async (axios) => await axios.get(ADMIN_URL);
 
 export const getAdminById = async (axios, id) =>
-  await axios.get(`${ADMIN_URL}/${id}`);
+  await axios.get(`${ADMIN_URL}/by-id/${id}`);
 
 export const createAdmin = async (axios, body) =>
   await axios.post(ADMIN_URL, body);
 
+export const restoreAdmin = async (axios, id) => {
+  return await axios.patch(`${ADMIN_URL}/${id}/restore`);
+};
+
+// TODO: right now archive admin is "delete", you will create a new delete?
 export const archiveAdmin = async (axios, id) => {
   return await axios.delete(`${ADMIN_URL}/${id}`);
 };
