1af61c1170d0fe2e7d5ad487a779d6c91f602564
amira tilouche <ati@visto-consulting.com>
2025-08-13 15:46:01 +0100

    feat(translation) : added a translation button

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,4 +1,3 @@
-// src/i18n.js
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
@@ -6,18 +5,20 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./locales/en.json";
 import fr from "./locales/fr.json";
 
+const resources = { en: { translation: en }, fr: { translation: fr } };
+
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      en: { translation: en },
-      fr: { translation: fr },
-    },
-    lng: "fr",
+    resources,
+    lng: "fr", 
     fallbackLng: "fr",
-    interpolation: {
-      escapeValue: false,
+    supportedLngs: ["fr", "en"],
+    interpolation: { escapeValue: false },
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
     },
   });
 
